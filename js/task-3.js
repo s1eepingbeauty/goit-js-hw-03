bcf213e832e8f7ba83e24bf6a6e33dc88b51586f
Elena <mazurenko_elena@i.ua>
2020-10-10 00:05:19 +0300

    сделала все задания

---
+++ js/task-3.js
@@ -0,0 +1,40 @@
+const findBestEmployee = function (employees) {
+    'use strict';
+    // Write code under this line
+    const keys = Object.keys(employees);
+    const values = Object.values(employees);
+    let maxValue = Math.max(...values);
+    let bestEmployee = '';
+    for (const key of keys) {
+      if(employees[key] === maxValue)
+      bestEmployee = key;
+    }
+    return bestEmployee;
+  };
+  
+  // Объекты и ожидаемый результат
+  const developers = {
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }; 
+  console.log(findBestEmployee(developers)); 
+  // 'lorence'
+  
+  const supports = {
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }; 
+  console.log(findBestEmployee(supports)); 
+  // 'mango'
+  
+  const sellers = {
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }
+  console.log(findBestEmployee(sellers)); 
+  // 'lux'  
---
+++ js/task-4.js
@@ -0,0 +1,28 @@
+const countTotalSalary = function(employees) {
+    'use strict';
+    // Write code under this line
+    const values = Object.values(employees);
+    let total = 0;
+    for (const value of values) {
+        total += value;
+    }
+    return total;
+  };
+  
+  // Объекты и ожидаемый результат
+  const developers = {
+      mango: 300,
+      poly: 250,
+      alfred: 450,
+  };
+  console.log(countTotalSalary(developers));
+  // 1000
+  
+  const supports = {
+    kiwi: 200,
+    lux: 150,
+    chelsy: 150,
+  }
+  console.log(countTotalSalary(supports));
+  // 500
+  
